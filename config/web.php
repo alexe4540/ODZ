c9b28bdcf9d22eb20b61a9fa9a94e087e7f81c38
Oleksii Shyshkin <shishkin.alexe@gmail.com>
2020-12-07 17:33:17 +0200

    Shishkin part

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'yE_GU67njNg1BAcFtRikqPoN_dRr8Ep2',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,21 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                ''=>'site/index',
+                '<action>'=>'site/<action>'
             ],
         ],
-        */
+
+    ],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
     ],
     'params' => $params,
 ];
